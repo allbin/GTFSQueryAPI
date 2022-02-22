fea35550abfe598f806f6c4306d64ad1a3071e6d
psc <samuel.olsson@allbinary.se>
2022-02-22 15:28:40 +0100

    Much improved query

--- query/query.go
+++ query/query.go
@@ -48,6 +48,53 @@ func (r *Repository) Connect(c config.DatabaseConfiguration) error {
 }
 
 func (r *Repository) GetStops(lat string, lon string, radius string, maxstops string) (*sql.Rows, error) {
+  return r.Db.Query("
+with target_stops as (
+  select * from stops where st_dwithin(
+    geography(st_point(stop_lon, stop_lat)),
+    geography(st_point(%s, %s)),
+    %s
+  )
+  order by
+    st_distance(
+      st_point(stop_lon, stop_lat),
+      st_point(%s, %s)
+    )
+  limit %s
+), cds as (
+  select * from calendar_dates where date in (
+    date(current_timestamp - interval '4 hours'),
+    date(current_timestamp + interval '20 hours')
+  )
+)
+SELECT
+  s.stop_id as id,
+  st.arrival_time,
+  st.departure_time,
+  s.stop_name as name,
+  s.stop_lat as lat,
+  s.stop_lon as lon,
+  trip_headsign as headsign,
+  coalesce(r.route_short_name, '') as short_name,
+  coalesce(r.route_long_name, '') as long_name,
+  date,
+  (
+    date :: varchar || ' ' || departure_time
+  ) as date_string
+from
+  target_stops s
+  inner join stop_times st on st.stop_id = s.stop_id
+  inner join trips t on st.trip_id = t.trip_id
+  inner join cds cd on t.service_id = cd.service_id
+  inner JOIN routes r on t.route_id = r.route_id
+order by
+  st_distance(
+    st_point(stop_lon, stop_lat),
+    st_point(%s, %s)
+  ),
+  id,
+  date_string", lon, lat, radius, lon, lat, max_stops, lon, lat)
+  /*
 	return r.Db.Query(
 		fmt.Sprintf("SELECT s.stop_id as id, arrival_time, departure_time, stop_name as name, stop_lat as lat, stop_lon as lon,"+
 			" trip_headsign as headsign, coalesce(r.route_short_name, '') as short_name, coalesce(r.route_long_name, '') as long_name, date, (date::varchar || ' ' || departure_time) as date_string"+
@@ -57,4 +104,5 @@ func (r *Repository) GetStops(lat string, lon string, radius string, maxstops st
 			" WHERE s.stop_id in (select stop_id from stops where st_dwithin(geography(st_point(stop_lat, stop_lon)), geography(st_point(%s,%s)), %s)"+
 			" order by st_distance(st_point(stop_lat, stop_lon), st_point(%s, %s)) limit %s) AND ((date(current_timestamp + interval '- 4 hours') = cd.date"+
 			" OR date(current_timestamp + interval '20 hours') = cd.date)) order by st_distance(st_point(stop_lat, stop_lon), st_point(%s, %s)), id, date_string;", lat, lon, radius, lat, lon, maxstops, lat, lon))
+  */
 }
